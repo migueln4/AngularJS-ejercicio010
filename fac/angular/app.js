1b0f07576ed46c577729f4fba0da8aa10b89427f
migueln4 <miguel444666@gmail.com>
2018-11-22 09:50:48 +0100

    Commit inicial
    Sin apuntes

---
+++ fac/angular/app.js
@@ -0,0 +1,128 @@
+var app = angular.module( 'facturacionApp',[ 
+		'ngRoute', 'jcs-autoValidate',
+
+		'facturacionApp.configuracion',
+		'facturacionApp.mensajes',
+		'facturacionApp.notificaciones',
+		'facturacionApp.clientes',
+		'facturacionApp.dashboardCrtl',
+		'facturacionApp.clientesCrtl'
+		]);
+// 7) Es muy importante que se vayan añadiendo en los corchetes las inyecciones de cada uno de los controladores que se vayan añadiendo a la aplicación.
+
+angular.module('jcs-autoValidate')
+.run([
+    'defaultErrorMessageResolver',
+    function (defaultErrorMessageResolver) {
+        // To change the root resource file path
+        defaultErrorMessageResolver.setI18nFileRootPath('angular/lib');
+        defaultErrorMessageResolver.setCulture('es-co');
+    }
+]);
+           
+
+
+app.controller('mainCtrl', ['$scope', 'Configuracion','Mensajes', 'Notificaciones', function($scope, Configuracion,Mensajes, Notificaciones){
+// 8) Hay que tener siempre en cuenta que hay que ir añadiendo aquí todoas las dependencias que se deben usar en el controlador.
+// 10) Cuando todos los servicios se han cargado en el controlador principal, ya están disponibles desde cualquier parte del programa.
+	
+	$scope.config = {}; //De primeras, se crea el objeto sin nada. Una vez que se ha creado, se produce la llamada al servicio.
+	$scope.mensajes = Mensajes.mensajes;
+	$scope.notificaciones = Notificaciones.notificaciones;
+
+	$scope.titulo    = "";
+	$scope.subtitulo = "";
+
+
+
+	$scope.usuario = {
+		nombre:"Fernando Herrera"
+	}
+
+
+
+
+	Configuracion.cargar().then( function(){ //Aquí es donde se guarda la configuración que se ha creado mucho más arriba. Es en este momento cuando ya se ha cargado toda la información del archivo configuracion.json.
+		$scope.config = Configuracion.config;
+	});
+
+
+	// ================================================
+	//   Funciones Globales del Scope
+	// ================================================
+	$scope.activar = function( menu, submenu, titulo, subtitulo ){
+
+		$scope.titulo    = titulo;
+		$scope.subtitulo = subtitulo;
+
+		$scope.mDashboard = "";
+		$scope.mClientes  = "";
+
+		$scope[menu] = 'active';
+
+	};
+
+
+
+}]);
+
+
+// ================================================
+//   Rutas
+// ================================================
+app.config([ '$routeProvider', function($routeProvider){
+
+// 3) Aquí es donde se configuran las rutas de cada uno de los archivos para que se apunte donde debería ir cada cosa.
+// 4) Muchas veces, aunque el enrutador no se haya definido aún, ya se especifica el nombre que tendrá. Eso se irá desarrollando poco a poco a medida que se avanza en la aplicación.
+
+	$routeProvider
+		.when('/',{
+			templateUrl: 'dashboard/dashboard.html',
+			controller: 'dashboardCtrl'
+		})
+		.when('/clientes/:pag',{
+			templateUrl: 'clientes/clientes.html',
+			controller: 'clientesCtrl'
+		})
+		.otherwise({
+			redirectTo: '/'
+		})
+
+}]);
+
+
+// ================================================
+//   Filtros
+// ================================================
+app.filter( 'quitarletra', function(){
+// 11) Tanto la parte de configuración, como la de enrutado, como esta de filtros, se pueden separar en archivos aparte e irlos incluyendo todos ellos en la página principal en el orden adecuado (el mismo que tienen aquí).
+
+	return function(palabra){
+		if( palabra ){
+			if( palabra.length > 1)
+				return palabra.substr(1);
+			else
+				return palabra;
+		}
+	}
+})
+
+.filter( 'mensajecorto', function(){ //Cada nuevo filtro que se vaya creando debe añadirse así, no hace falta volver a poner lo de app. porque son diferentes filtros que se van sumando al objeto en sí.
+
+	return function(mensaje){
+		if( mensaje ){
+			if( mensaje.length > 35)
+				return mensaje.substr(0,35) + "...";
+			else
+				return mensaje;
+		}
+	}
+})
+
+
+
+
+
+
+
+
---
+++ fac/angular/servicios/configuracion_service.js
@@ -0,0 +1,40 @@
+var app = angular.module('facturacionApp.configuracion',[]);
+// 6) En este archivo se va a controlar que se pueda cargar el archivo de configuración .json que se ha hecho expresamente para introducir toda la información que tiene.
+
+
+app.factory('Configuracion', ['$http', '$q', function($http, $q){
+// 9) Esta lectura se hace con promesas y peticiones HTTP.
+
+	var self = {
+
+		config:{}, //De primeras, no tiene nada porque hay que leer el archivo .json
+		cargar: function(){
+
+			var d = $q.defer();
+
+			$http.get('configuracion.json')
+				.success(function(data){
+
+					self.config = data;
+					d.resolve();
+
+
+				})
+				.error(function(){
+
+					d.reject();
+					console.error("No se pudo cargar el archivo de configuración");
+
+				});
+
+			return d.promise;
+		}
+
+
+
+	};
+
+
+	return self;
+
+}])
